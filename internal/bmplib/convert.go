dbe1608fa9d6d98e06e6bfd04214d96ef3429dbc
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2024-04-16 09:18:35 +0300

    Added functions to convert bmp

--- internal/bmplib/convert.go
+++ internal/bmplib/convert.go
@@ -1,11 +1,52 @@
 package bmplib
 
 import (
+	"fmt"
 	"image"
+	"image/jpeg"
+	"image/png"
 	"os"
 
 	"golang.org/x/image/bmp"
 )
+
+func Bmp2Jpeg(fName string, outFName string) error {
+	img, err := bmp2Image(fName)
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	if err != nil {
+		panic(err)
+	}
+	outFile, err := os.Create(outFName)
+	if err != nil {
+		panic(err)
+	}
+	defer outFile.Sync()
+	defer outFile.Close()
+	return jpeg.Encode(outFile, img, &jpeg.Options{Quality: 90})
+}
+
+func Bmp2Png(fName string, outFName string) error {
+	img, err := bmp2Image(fName)
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	if err != nil {
+		panic(err)
+	}
+	outFile, err := os.Create(outFName)
+	if err != nil {
+		panic(err)
+	}
+	defer outFile.Sync()
+	defer outFile.Close()
+	return png.Encode(outFile, img)
+}
 
 func bmp2Image(fName string) (image.Image, error) {
 	file, err := os.Open(fName)
